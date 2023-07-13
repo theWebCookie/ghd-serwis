c2d13dae63fc2b3b3a797def18ce707edd4cb482
theWebCookie <jabwojtek@wp.pl>
2023-07-13 20:15:05 +0200

    connect cms with repair page

--- src/components/pages/RepairPage.jsx
+++ src/components/pages/RepairPage.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useQuery } from "graphql-hooks";
 import Heading from '../parts/Heading';
 import RepairBox from '../parts/RepairBox';
 
-const RepairPage = () => (
-  <div className="repairSection">
-    <Heading content="repair" />
-    <div className="repairBoxes">
-      <RepairBox />
+const REPAIR_QUERY = `query MyQuery {
+  allRepairs {
+    cena
+    id
+    lista
+    nazwa
+  }
+}`;
+
+const RepairPage = () => {
+  const { data } = useQuery(REPAIR_QUERY);
+  const repairs = data?.allRepairs;
+
+  return (
+    <div className="repairSection">
+      <Heading content="repair" />
+      <div className="repairBoxes">
+      {repairs?.map((repair) => (
+          <RepairBox key={repair.id} data={repair} />
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 export default RepairPage;

fe54fd6e2f6baeea2420f7e685f9604a2cf79ad6
theWebCookie <jabwojtek@wp.pl>
2023-07-13 22:10:59 +0200

    create offert-input

--- src/components/pages/OffertsPage.jsx
+++ src/components/pages/OffertsPage.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useQuery } from "graphql-hooks";
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'graphql-hooks';
 import Heading from '../parts/Heading';
 import OffertBox from '../parts/OffertBox';
-import "../../styles/OffertBox.css"
+import '../../styles/OffertBox.css';
 
 const OFFERT_QUERY = `query MyQuery {
   allSells {
@@ -13,7 +13,7 @@ const OFFERT_QUERY = `query MyQuery {
     zuzycie
     zdjecie {
       alt
-      responsiveImage(imgixParams: {w: "200", h: "200"}) {
+      responsiveImage(imgixParams: { w: "200", h: "200" }) {
         alt
         src
       }
@@ -24,16 +24,33 @@ const OFFERT_QUERY = `query MyQuery {
 const OffertsPage = () => {
   const { data } = useQuery(OFFERT_QUERY);
   const offerts = data?.allSells;
+  const [filteredOfferts, setFilteredOfferts] = useState(offerts || []);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    const result = offerts.filter((offert) => offert.nazwa.toLowerCase().includes(value.toLowerCase()));
+    setFilteredOfferts(result);
+  };
+
+  useEffect(() => {
+    if (offerts) {
+      setFilteredOfferts(offerts);
+    }
+  }, [offerts]);
+
   return (
     <div className="offertSection">
       <Heading content="offert" />
+      <div className="offertInput">
+        <input type="text" name="searchInput" placeholder="Szukaj..." className="searchInput" onChange={handleInputChange} />
+      </div>
       <div className="offertBoxes">
-        {offerts?.map((offert) => (
+        {filteredOfferts?.map((offert) => (
           <OffertBox key={offert.id} data={offert} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default OffertsPage;

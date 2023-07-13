538f2e0bbe51949c15ecb54add38bebf21dd9c8c
theWebCookie <jabwojtek@wp.pl>
2023-07-13 21:29:32 +0200

    connect cms with offert page

--- src/components/pages/OffertsPage.jsx
+++ src/components/pages/OffertsPage.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { useQuery } from "graphql-hooks";
+import Heading from '../parts/Heading';
+import OffertBox from '../parts/OffertBox';
+import "../../styles/OffertBox.css"
 
-const OffertsPage = () => (
-  <div>Oferty</div>
-)
+const OFFERT_QUERY = `query MyQuery {
+  allSells {
+    nazwa
+    link
+    id
+    cena
+    zuzycie
+    zdjecie {
+      alt
+      responsiveImage(imgixParams: {w: "200", h: "200"}) {
+        alt
+        src
+      }
+    }
+  }
+}`;
+
+const OffertsPage = () => {
+  const { data } = useQuery(OFFERT_QUERY);
+  const offerts = data?.allSells;
+  return (
+    <div className="offertSection">
+      <Heading content="offert" />
+      <div className="offertBoxes">
+        {offerts?.map((offert) => (
+          <OffertBox key={offert.id} data={offert} />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default OffertsPage;
---
+++ src/components/parts/OffertBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const OffertBox = ({data}) => {
+  const { nazwa, link, cena, zuzycie, zdjecie } = data;
+  const { responsiveImage } = zdjecie;
+  return (
+    <div className="offertBox">
+      <img src={responsiveImage.src} alt={responsiveImage.alt} />
+      <div>
+        <span className="name">{nazwa}</span>
+        <span className="price">{cena}zł</span>
+      </div>
+      <div>
+        <span>Zużycie: {zuzycie}</span>
+        <Link to={link}>Sprawdź</Link>
+      </div>
+    </div>
+  )
+}
+
+export default OffertBox;

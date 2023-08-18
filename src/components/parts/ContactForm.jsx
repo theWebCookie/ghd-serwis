816acb01e8aeacdeb089346437f37d2ad449e31e
theWebCookie <jabwojtek@wp.pl>
2023-08-18 20:11:25 +0200

    refactor ContactForm component

--- src/components/parts/ContactForm.jsx
+++ src/components/parts/ContactForm.jsx
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import Input from './Input';
 import Alert from './Alert';
 import style from '../../styles/ContactForm.css';
 
 const ContactForm = () => {
-  const [alertMessage, setAlertMessage] = useState("");
+  const [alertMessage, setAlertMessage] = useState('');
   const [showAlert, setShowAlert] = useState(false);
+  const formRef = useRef(null);
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    const form = e.target;
+
+    const form = formRef.current;
     const isValid = form.checkValidity();
 
     form.classList.add('submitted');
@@ -19,57 +21,32 @@ const ContactForm = () => {
 
     if (!isValid) return;
 
-    emailjs
-    .sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form, process.env.REACT_APP_PUBLIC_KEY)
-    .then(() => {
-        setAlertMessage("success");
-        setShowAlert(true);
-    }, () => {
-        setAlertMessage("failure");
-        setShowAlert(true);
-    });
+    try {
+      await emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form, process.env.REACT_APP_PUBLIC_KEY);
+      setAlertMessage('success');
+    } catch (error) {
+      setAlertMessage('failure');
+    } finally {
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 5000);
+    }
 
-    setTimeout(() => {
-      setShowAlert(false);
-    }, 5000);
+    form.reset();
   };
 
   return (
     <>
-      <form onSubmit={handleFormSubmit} noValidate className={style.form}>
-        <div className="formWrapper">
-          <Input
-            component="input"
-            type="text"
-            name="user_name"
-            className="contactField"
-            placeholder="Imię i nazwisko"
-            required
-          />
-          <Input
-            component="input"
-            type="email"
-            name="user_email"
-            className="contactField"
-            placeholder="Email"
-            required
-          />
-          <Input
-            component="textarea"
-            name="message"
-            className="contactField"
-            placeholder="Wiadomość"
-            required
-          />
+      <form ref={formRef} onSubmit={handleFormSubmit} noValidate className={style.form}>
+        <div className='formWrapper'>
+          <Input component='input' type='text' name='user_name' className='contactField' placeholder='Imię i nazwisko' required />
+          <Input component='input' type='email' name='user_email' className='contactField' placeholder='Email' required />
+          <Input component='textarea' name='message' className='contactField' placeholder='Wiadomość' required />
         </div>
-        <Input
-            component="input"
-            type="submit"
-            className="contactField"
-            value="Wyślij"
-          />
+        <Input component='input' type='submit' className='contactField' value='Wyślij' />
       </form>
-      {showAlert ? <Alert type={alertMessage}/> : ""}
+      {showAlert && <Alert type={alertMessage} />}
     </>
   );
 };

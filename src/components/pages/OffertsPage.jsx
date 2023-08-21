8e563973cc7e4d56a4dc0d077a86ae7be0f9d375
theWebCookie <jabwojtek@wp.pl>
2023-08-21 17:21:34 +0200

    update OffertsPage

--- src/components/pages/OffertsPage.jsx
+++ src/components/pages/OffertsPage.jsx
@@ -3,21 +3,25 @@ import useOffertsPage from '../../hooks/useOffertsPage';
 import Heading from '../parts/Heading';
 import OffertBox from '../parts/OffertBox';
 import '../../styles/OffertBox.css';
+import Nav from '../parts/Nav';
 
 const OffertsPage = () => {
   const { filteredOfferts, handleInputChange } = useOffertsPage();
   const isLoading = !filteredOfferts;
 
   return (
-    <div className='offertSection'>
-      <Heading content='offert' />
-      <div className='offertInput'>
-        <input type='text' name='searchInput' placeholder='Szukaj...' className='searchInput' onChange={handleInputChange} />
+    <>
+      <Nav />
+      <div className='offertSection'>
+        <Heading content='offert' />
+        <div className='offertInput'>
+          <input type='text' name='searchInput' placeholder='Szukaj...' className='searchInput' onChange={handleInputChange} />
+        </div>
+        <div className='offertBoxes'>
+          {isLoading ? <p>Ładowanie ofert...</p> : filteredOfferts.map((offert) => <OffertBox key={offert.id} data={offert} />)}
+        </div>
       </div>
-      <div className='offertBoxes'>
-        {isLoading ? <p>Ładowanie ofert...</p> : filteredOfferts.map((offert) => <OffertBox key={offert.id} data={offert} />)}
-      </div>
-    </div>
+    </>
   );
 };
 

1cfac458b5b8fa6f226cd6d4e55735eb2ed76ff5
theWebCookie <jabwojtek@wp.pl>
2023-08-23 14:47:03 +0200

    optimize website

--- src/components/parts/Heading.jsx
+++ src/components/parts/Heading.jsx
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from "react";
-import "../../styles/Heading.css";
+import React, { useState, useEffect } from 'react';
+import '../../styles/Heading.css';
 
 const Heading = ({ content }) => {
-  const [text, setText] = useState({ title: "", paragraph: "" });
+  const [text, setText] = useState({ title: '', paragraph: '' });
 
   useEffect(() => {
     const repairText = {
-      title: "Oferta naprawy",
-      paragraph: "Na wszystkie naprawy udzielamy 6 miesięcy gwarancji. W ramach naprawy wymieniamy pasty termoprzewodzące. Koszt diagnozy wynosi 30zł, jest pomijany w przypadku decyzji naprawy."
-    };
-  
-    const offertText = {
-      title: "Nasze oferty sprzedaży",
-      paragraph: "Swoją sprzedaż prowadzimy na portalu olx oraz allegro. Wszytskie oferowane przez nas urządzenie zostają sprawdzone przed wysyłką. Przechodzą pełną konserwację. (czyszczenie ze śladów kosmetyków, oraz wymiana past termoprzewodzących). W cenie zakupu oferujemy rok gwarancji, z automatycznym przedłużeniem w w przypadku awarii."
+      title: 'Oferta naprawy',
+      paragraph:
+        'Na wszystkie naprawy udzielamy 6 miesięcy gwarancji. W ramach naprawy wymieniamy pasty termoprzewodzące. Koszt diagnozy wynosi 30zł, jest pomijany w przypadku decyzji naprawy.',
     };
 
-    setText(content === "repair" ? repairText : offertText);
+    const offertText = {
+      title: 'Nasze oferty sprzedaży',
+      paragraph:
+        'Swoją sprzedaż prowadzimy na portalu olx oraz allegro. Wszystkie oferowane przez nas urządzenie zostają sprawdzone przed wysyłką. Przechodzą pełną konserwację. (czyszczenie ze śladów kosmetyków, oraz wymiana past termoprzewodzących). W cenie zakupu oferujemy rok gwarancji, z automatycznym przedłużeniem w w przypadku awarii.',
+    };
+
+    setText(content === 'repair' ? repairText : offertText);
   }, [content]);
 
   return (
-    <div className="heading">
+    <div className='heading'>
       <h1>{text.title}</h1>
       <p>{text.paragraph}</p>
     </div>
   );
-}
+};
 
 export default Heading;

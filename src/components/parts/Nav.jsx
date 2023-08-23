0656e8772a00b2aa055d333fb53ce9e4af50db13
theWebCookie <jabwojtek@wp.pl>
2023-08-23 18:54:16 +0200

    add nav logo

--- src/components/parts/Nav.jsx
+++ src/components/parts/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
 
   useEffect(() => {
     setIsMobileMenuOpen(false);
-  }, [location])
+  }, [location]);
 
   return (
     <nav className='nav'>
@@ -23,28 +23,38 @@ const Nav = () => {
         </NavLink>
       </div>
       <div className={isMobileMenuOpen ? 'mobile open' : 'mobile close'}>
+        <div>
+          <NavLink to='/' className='nav-logo'>
+            <span>GHD</span>
+            <span>SERVICE</span>
+          </NavLink>
+        </div>
         <NavLink to='/naprawa' className='nav-link'>
           Naprawa
-        </NavLink><NavLink to='/zakup' className='nav-link'>
+        </NavLink>
+        <NavLink to='/zakup' className='nav-link'>
           Zakup
-        </NavLink><NavLink to='/regulamin' className='nav-link'>
+        </NavLink>
+        <NavLink to='/regulamin' className='nav-link'>
           Regulamin
         </NavLink>
       </div>
-      <div className="menu">
-          <NavLink to='/naprawa' className='nav-link'>
-            Naprawa
-          </NavLink><NavLink to='/zakup' className='nav-link'>
-            Zakup
-          </NavLink><NavLink to='/regulamin' className='nav-link'>
-            Regulamin
-          </NavLink>
-        </div>
+      <div className='menu'>
+        <NavLink to='/naprawa' className='nav-link'>
+          Naprawa
+        </NavLink>
+        <NavLink to='/zakup' className='nav-link'>
+          Zakup
+        </NavLink>
+        <NavLink to='/regulamin' className='nav-link'>
+          Regulamin
+        </NavLink>
+      </div>
       {isMobileMenuOpen ? (
         <i className='fa-solid fa-x x' onClick={handleNavBarClick}></i>
-        ) : (
-            <i className='fa-solid fa-bars bars' onClick={handleNavBarClick}></i>
-        )}
+      ) : (
+        <i className='fa-solid fa-bars bars' onClick={handleNavBarClick}></i>
+      )}
     </nav>
   );
 };
